135a8fc88b09bfb03f9fa42a943df1bc9dd1d127
Mike Moore <mike@blowmage.com>
2025-01-16 18:45:32 -0700

    Update benchmarks
    * Fix Phlex benchmark class.
    * Iterate in benchmark report, not class.
      * This allows for optimizations to be used.
    * Increase the benchmark iterations.

--- benchmark/benchmark.rb
+++ benchmark/benchmark.rb
@@ -1,84 +1,99 @@
 # frozen_string_literal: true
 
-require "byebug"
 require "benchmark"
+require "byebug"
 require "erubi"
-require "html_slice"
+require "markaby"
 require "papercraft"
 require "phlex"
-require "markaby"
+require "html_slice"
 
-ANY = 100
-
-class TestHtmlSlice
-  include HtmlSlice
+class RunErubi
+  def initialize text = "Benchmark"
+    @text = text
+  end
 
   def call
-    html_layout do
-      ANY.times do
-        div do
-          h1 "Benchmark"
-        end
+    eval Erubi::Engine.new("<div><h1><%= @text %></h1></div>").src
+  end
+end
+
+class RunMarkaby
+  def initialize text = "Benchmark"
+    @text = text
+  end
+
+  def call
+    Markaby::Builder.new text: @text do
+      div do
+        h1 @text
+      end
+    end.to_s
+  end
+end
+
+class RunPapercraft
+  def initialize text = "Benchmark"
+    @text = text
+  end
+
+  def call
+    Papercraft.html do |text:|
+      div do
+        h1 text
+      end
+    end.render text: @text
+  end
+end
+
+class RunPhlex < Phlex::HTML
+  def initialize text = "Benchmark"
+    @text = text
+  end
+
+  def view_template
+    div do
+      h1 { @text }
+    end
+  end
+end
+
+class RunHtmlSlice
+  include HtmlSlice
+
+  def initialize text = "Benchmark"
+    @text = text
+  end
+
+  def call
+    html_slice do
+      div do
+        h1 @text
       end
     end
   end
 end
 
-class TestPapercraft
-  def call
-    Papercraft.html {
-      ANY.times do
-        div do
-          h1 "Benchmark"
-        end
-      end
-    }.render
-  end
-end
-
-class TestMarkaby
-  def call
-    Markaby::Builder.new.html {
-      ANY.times do
-        div do
-          h1 "Benchmark"
-        end
-      end
-    }.to_s
-  end
-end
-
-#Can't make it work outside rails =/
-
-#class TestPhlex < Phlex::HTML
-  #def view_template
-    #ANY.times do
-      #h1 "Benchmark"
-    #end
-  #end
-#end
+ITERATIONS = 100_000
 
 Benchmark.bm do |x|
-  x.report("papercraft") do
-    TestPapercraft.new.call
+  x.report("erubi") do
+    ITERATIONS.times { |count| RunErubi.new("Benchmark #{count}").call }
   end
 
   x.report("markaby") do
-    TestMarkaby.new.call
+    ITERATIONS.times { |count| RunMarkaby.new("Benchmark #{count}").call }
   end
 
-  x.report("(erubi) parsing an erb string") do
-    eval(Erubi::Engine.new("
-<% ANY.times do %>
-  <h1> Benchmark </h1>
-<% end %>").src)
+  x.report("papercraft") do
+    ITERATIONS.times { |count| RunPapercraft.new("Benchmark #{count}").call }
   end
 
-  x.report("(erubi) reading and parsing an erb file") do
-    eval(Erubi::Engine.new(File.read("index.html.erb")).src)
+  x.report("phlex") do
+    ITERATIONS.times { |count| RunPhlex.new("Benchmark #{count}").call }
   end
 
   x.report("html_slice") do
-    TestHtmlSlice.new.call
+    ITERATIONS.times { |count| RunHtmlSlice.new("Benchmark #{count}").call }
   end
 end
